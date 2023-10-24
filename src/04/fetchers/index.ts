fd42258b97b5209043d0e572cb00be30137915e2
ShinEndo <endo@elplanning.co.jp>
2023-10-24 11:34:23 +0900

    第4章　モック

--- src/04/fetchers/index.ts
+++ src/04/fetchers/index.ts
@@ -1,11 +1,26 @@
-import { Profile } from './type';
+import { Article, ArticleInput, Articles, Profile } from './type';
+
+async function handleResponse(res: Response) {
+	const data = await res.json();
+	if (!res.ok) {
+		throw data;
+	}
+	return data;
+}
+
+const host = (path: string) => `http://myapi.testing.com${path}`;
 
 export function getMyProfile(): Promise<Profile> {
-	return fetch('https://myapi.testing.com/my/profile').then(async (res) => {
-		const data = await res.json();
-		if (!res.ok) {
-			throw data;
-		}
-		return data;
-	});
+	return fetch(host('/my/profile')).then(handleResponse);
+}
+
+export function getMyArticles(): Promise<Articles> {
+	return fetch(host('/my/articles')).then(handleResponse);
+}
+
+export function postMyArticle(input: ArticleInput): Promise<Article> {
+	return fetch(host('/my/articles'), {
+		method: 'POST',
+		body: JSON.stringify(input),
+	}).then(handleResponse);
 }
